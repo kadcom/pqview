354926140e688dd3de4cb2451a75c0442938bd89
Clair Guang <lynxluna@gmail.com>
2025-10-10 04:04:35 +0700

    Move comctl32 and manifest linker options from pragma to CMakeLists.txt
    Move build configuration from source code to build system:
    - Remove pragma comment(lib, comctl32.lib) from all main.c files
    - Remove pragma comment(linker, manifestdependency) from all main.c
    - Add target_link_options() in CMakeLists.txt for manifest embedding
    - comctl32 already linked via target_link_libraries in CMakeLists.txt
    - Applies to: hour2.2, hour3, hour3.1, hour4, hour4.1
    - Cleaner separation: build config in CMake, code in C files
    - Makes cross-platform builds easier in future

--- hour4_listview_results/main.c
+++ hour4_listview_results/main.c
@@ -7,9 +7,6 @@
 #include <string.h>
 #include <time.h>
 
-#pragma comment(lib, "comctl32.lib")
-#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-
 /* Window class name */
 #define WINDOW_CLASS_NAME "pqview"
 
